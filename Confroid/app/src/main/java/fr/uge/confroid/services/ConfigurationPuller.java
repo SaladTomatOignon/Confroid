276cf7ee97b9916b8ff35e17ffd05f685d6db35a
Mamadou Cisse <mciissee@gmail.com>
2020-12-15 22:32:01 +0100

    feat: add configuration puller service skeleton

--- Confroid/app/src/main/java/fr/uge/confroid/services/ConfigurationPuller.java
+++ Confroid/app/src/main/java/fr/uge/confroid/services/ConfigurationPuller.java
@@ -1,4 +1,16 @@
 package fr.uge.confroid.services;
 
-public class ConfigurationPuller {
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+public class ConfigurationPuller extends BackgroundService {
+    private static final String TAG = "ConfigurationPuller";
+
+    public ConfigurationPuller() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) { }
 }
