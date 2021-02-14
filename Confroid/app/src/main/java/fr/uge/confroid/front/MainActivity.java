a4f60c921ed6da9a03508467f681d84a6da074b6
Volkan ZULAL <43728152+volkanzulal@users.noreply.github.com>
2021-02-14 10:55:11 +0100

    Revert "récupère les données d’un type blob"
    This reverts commit e72c65df497f207e5fe284c6766e1fda45779b07.
    
    # Conflicts:
    #	Confroid/app/src/main/java/fr/uge/confroid/front/MenuConfigurationPrinter.java

--- Confroid/app/src/main/java/fr/uge/confroid/front/MainActivity.java
+++ Confroid/app/src/main/java/fr/uge/confroid/front/MainActivity.java
@@ -2,10 +2,7 @@ package fr.uge.confroid.front;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import fr.uge.confroid.R;
 
@@ -14,15 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.buttonToMenu);
-        Intent intent = new Intent(this, MenuConfigurationPrinter.class);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), MenuConfigurationPrinter.class);
-                v.getContext().startActivity(intent);
-            }
-        });
-
     }
 }
