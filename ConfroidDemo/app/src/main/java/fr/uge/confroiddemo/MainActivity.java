3eca55ecbecd056b0f665b29f71059a99bdb2d83
Samy WADAN <SaladeTomatOignon@hotmail.com>
2021-04-01 14:27:13 +0200

    refactor: using a scrollable layout for app demo

--- ConfroidDemo/app/src/main/java/fr/uge/confroiddemo/MainActivity.java
+++ ConfroidDemo/app/src/main/java/fr/uge/confroiddemo/MainActivity.java
@@ -1,8 +1,14 @@
 package fr.uge.confroiddemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.ActivityNotFoundException;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import java.util.List;
 import java.util.Map;
@@ -15,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle("Confroid application test");
 
         // Creating some objects ... (including primitives types)
         ShoppingPreferences prefs = DemoUtils.createShoppingPreferences();
@@ -32,9 +40,28 @@ public class MainActivity extends AppCompatActivity {
             ConfroidUtils.saveConfiguration(this, "primitive", primitive, null);
         });
 
-        // Launch Confroid editing activity for the ShoppingPreferences object
+        // Launch Confroid editing activity for the ShoppingPreferences object and edit it (in local)
         findViewById(R.id.edit).setOnClickListener(view -> {
-            ConfroidUtils.editObject(this, prefs, (editedPrefs) -> { /* Do what you want */ });
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                Toast.makeText(this, "Maximum authorized SDK version : 28", Toast.LENGTH_SHORT).show();
+            }
+            try {
+                ConfroidUtils.editObject(this, prefs, (editedPrefs) -> { /* Do what you want */ });
+            } catch (ActivityNotFoundException anfe) {
+                Toast.makeText(this, "You need to install Confroid first", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Launch Confroid editing activity for the ShoppingPreferences object and edit (then update the database)
+        findViewById(R.id.update).setOnClickListener(view -> {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                Toast.makeText(this, "Maximum authorized SDK version : 28", Toast.LENGTH_SHORT).show();
+            }
+            try {
+                ConfroidUtils.updateObject(this, "shoppingPreferences", "stable", (editedPrefs) -> { /* Do what you want */ });
+            } catch (ActivityNotFoundException anfe) {
+                Toast.makeText(this, "You need to install Confroid first", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
